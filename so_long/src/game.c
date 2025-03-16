c518e5d1dee0e98d8fccd1e97e87f15151bd2163
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2025-03-16 00:19:38 -0300

    define sprite size

--- so_long/src/game.c
+++ so_long/src/game.c
@@ -9,15 +9,19 @@ void init_game(t_game *game) {
     if (!game->win)
         exit(printf("Error: Could not create window!\n"));
 
+    int sprite_width = 32;
+    int sprite_height = 32;
+
     // Carrega os sprites
+    //TODO: Verificar se é possível usar um valor dinamico para sprite_width e sprite_height
     game->floor_img = mlx_xpm_file_to_image(game->mlx, "assets/floor.xpm", &sprite_width, &sprite_height);
     game->wall_img = mlx_xpm_file_to_image(game->mlx, "assets/wall.xpm", &sprite_width, &sprite_height);
     game->player_img = mlx_xpm_file_to_image(game->mlx, "assets/player.xpm", &sprite_width, &sprite_height);
     game->collectible_img = mlx_xpm_file_to_image(game->mlx, "assets/collectible.xpm", &sprite_width, &sprite_height);
     game->exit_img = mlx_xpm_file_to_image(game->mlx, "assets/exit.xpm", &sprite_width, &sprite_height);
 
-    if (!game->grass_img || !game->tree_img || !game->player_img || !game->collectible_img || !game->exit_img)
+    if (!game->floor_img || !game->wall_img || !game->player_img || !game->collectible_img || !game->exit_img)
         exit(printf("Erro: Não foi possível carregar os sprites!\n"));
-
+    //TODO: Caso decida contabilizar os movimentos
     //game->moves = 0;
 }
