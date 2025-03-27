0f3de13cb0223f705662d2d9761181cc3f828e18
rdos-san <raquelv.jacques@gmail.com>
2025-03-27 07:41:48 -0300

    fix: remove comments and add destroy all

--- so_long/src/events.c
+++ so_long/src/events.c
@@ -2,41 +2,33 @@
 
 
 static void move_player(t_game *game, int new_x, int new_y) {
-    // Verificar se o movimento é para fora do mapa não faz sentido pq a borda estará preenchida
-
-    // Verifica se o jogador está tentando atravessar uma parede
     if (game->map[new_y][new_x] == '1')
     {
         return;
     }
-    // Se pisar em um coletável, decrementa o contador
     if (game->map[new_y][new_x] == 'C') {
         game->collectibles--;
     }
 
-    // Se pisar na saída e todos os coletáveis foram pegos, fecha o jogo
     if (game->map[new_y][new_x] == 'E' && game->collectibles == 0) {
         printf("Congratulations! You won the game!\n");
-		// TODO: Dar free na matriz inteira
         destroy_all(game);
-    } else if (game->map[new_y][new_x] == 'E')
+        return;
+    } 
+    else if (game->map[new_y][new_x] == 'E')
     {
         return;
     }
-    // Atualiza a posição do jogador no mapa
-    game->map[game->player_y][game->player_x] = '0'; //Coloca o chão na posição antiga
-    game->map[new_y][new_x] = 'P'; // Atualiza a posição do jogador
+    game->map[game->player_y][game->player_x] = '0';
+    game->map[new_y][new_x] = 'P';
     game->player_x = new_x;
     game->player_y = new_y;
-
-    // Renderiza o mapa após o movimento
     render_map(game);
 }
 
 int handle_keypress(int keycode, t_game *game) {
     if (keycode == ESC) {
-        mlx_destroy_window(game->mlx, game->win);
-        exit(0); // Fecha o jogo
+        destroy_all(game);
     } else if (keycode == KEY_W) {
         move_player(game, game->player_x, game->player_y - 1); 
     } else if (keycode == KEY_S) {
@@ -46,12 +38,12 @@ int handle_keypress(int keycode, t_game *game) {
     } else if (keycode == KEY_D) {
         move_player(game, game->player_x + 1, game->player_y);
     }
-    return 0; // Retorna 0 para indicar que o evento foi tratado
+    return 0;
 }
+
 
 int	handle_exit(t_game *game)
 {
-	mlx_destroy_window(game->mlx, game->win);
-	exit(0);
+	destroy_all(game);
 	return (0);
 }
