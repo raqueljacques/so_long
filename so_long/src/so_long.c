401008df63f2e3485fa3892373e6407289e6f560
rdos-san <raquelv.jacques@gmail.com>
2025-03-26 23:25:48 -0300

    fix: norma and change error message

--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -1,27 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdos-san <rdos-san@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 23:20:35 by rdos-san          #+#    #+#             */
+/*   Updated: 2025/03/26 23:22:18 by rdos-san         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/so_long.h"
 
 int	main(int argc, char **argv)
 {
-    t_game game;
-    if (argc != 2)
-    {
-        ft_printf("Error: invalid number of arguments\n");
-        return (1);
-    }
-    if (!load_map(argv[1], &game))
-    {
-        ft_printf("Error: invalid map\n");
-        return (1);
-    }
-    // Inicializa o jogo (carrega mapa, inicializa MiniLibX, etc.)
-    init_game(&game);
+	t_game	game;
 
-    // Renderiza o mapa na tela pela primeira vez
-    render_map(&game);
-
-    //Registrar eventos
-    mlx_key_hook(game.win, handle_keypress, &game);
-    mlx_hook(game.win, CLOSE_WINDOW, 0, handle_exit, &game);
-    mlx_loop(game.mlx);
-    return (0);
+	if (argc != 2)
+		exit(printf("Error: invalid number of arguments\n"));
+	if (!validate_file(argv[1]) || !load_map(argv[1], &game))
+		exit(printf("Error: invalid type of file or map\n"));
+	init_game(&game);
+	render_map(&game);
+	mlx_key_hook(game.win, handle_keypress, &game);
+	mlx_hook(game.win, CLOSE_WINDOW, 0, handle_exit, &game);
+	mlx_loop(game.mlx);
+	return (0);
 }
