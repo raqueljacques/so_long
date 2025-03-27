9684647cfd6962c5ddfb1c99c152e0486a5a4b0a
rdos-san <raquelv.jacques@gmail.com>
2025-03-27 07:37:30 -0300

    fix: remove comments

--- so_long/src/render.c
+++ so_long/src/render.c
@@ -5,17 +5,16 @@ void render_map(t_game *game) {
 
     for (y = 0; y < game->height; y++) {
         for (x = 0; x < game->width; x++) {
-            if (game->map[y][x] == '1')  // Parede
+            if (game->map[y][x] == '1')
                 mlx_put_image_to_window(game->mlx, game->win, game->wall_img, x * 32, y * 32);
-            else if (game->map[y][x] == '0')  // Chão
+            else if (game->map[y][x] == '0')
                 mlx_put_image_to_window(game->mlx, game->win, game->floor_img, x * 32, y * 32);
-            else if (game->map[y][x] == 'C')  // Colecionável
+            else if (game->map[y][x] == 'C')
                 mlx_put_image_to_window(game->mlx, game->win, game->collectible_img, x * 32, y * 32);
-            else if (game->map[y][x] == 'E')  // Saída
+            else if (game->map[y][x] == 'E')
                 mlx_put_image_to_window(game->mlx, game->win, game->exit_img, x * 32, y * 32);
         }
     }
     
-    // Desenha o jogador na nova posição
     mlx_put_image_to_window(game->mlx, game->win, game->player_img, game->player_x * 32, game->player_y * 32);
 }
